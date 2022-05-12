87d51bec7fe0b483565a51ba7b7f6763ee6235ec
Dario-Caf <cafasso.dario@gmail.com>
2022-05-13 00:51:51 +0200

    first test upload

---
+++ Tests/test.py
@@ -0,0 +1,13 @@
+"""Test for the core.py program."""
+
+import unittest
+
+class TestCore(unittest.TestCase):
+    """Test methods class."""
+
+    def test_float(self):
+        """Square test."""
+        self.assertAlmostEqual(square(2.), 4.)
+
+if __name__ == "__main__":
+    unittest.main()
