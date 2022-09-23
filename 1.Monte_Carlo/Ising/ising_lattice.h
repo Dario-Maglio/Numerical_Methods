4eb4446121b9bfa53401c2517f6809a00cac6eba
Dario-Caf <cafasso.dario@gmail.com>
2022-09-23 20:46:03 +0300

    ising upgrade, basic class completed and tested

--- 1.Monte_Carlo/Ising/ising_lattice.h
+++ 1.Monte_Carlo/Ising/ising_lattice.h
@@ -1,8 +1,10 @@
-/**
+/*******************************************************************************
+*
 * Lattice class definition
-*/
+*
+*******************************************************************************/
 
-//----Preprocessor directives--------------------------------------
+//----Preprocessor directives---------------------------------------------------
 #ifndef ISING_LATTICE_H
 #define ISING_LATTICE_H
 
@@ -13,9 +15,15 @@
 #include <random>
 #include <cmath>
 
+#define SEED 42
+
 using namespace std;
 
-//----Contents-----------------------------------------------------
+//----Contents------------------------------------------------------------------
+
+float random_number;
+mt19937 generator(SEED);
+uniform_real_distribution<double> prng(0.0, 1.0);
 
 class lattice {
 private:
@@ -24,65 +32,174 @@ private:
     vector<vector<int>> nearest_neighbors_;
 
 public:
-    const int side_lenght, temperature_flag, geometry_flag;
-    lattice(const int &L = 5, const int &G_FLAG = 0, const int &T_FLAG = 0):
+    const int side_lenght, geometry_flag, initial_flag;
+    lattice(const int &L = 5, const int &G_FLAG = 0, const int &I_FLAG = 0):
         side_lenght(L),
-        temperature_flag(T_FLAG),
-        geometry_flag(G_FLAG)
-    {
-        /* Initialising the lattice */
+        geometry_flag(G_FLAG),
+        initial_flag(I_FLAG)
+    {/************************** Class constructor ****************************/
+
+        // Defining topology and nearest neighbors list
+
         vector<int> nearest_list;
+
         if (geometry_flag == 0) {
-            /* Initialising 1D lattice and nearest neighbors */
+            /* 1D lattice with PBC */
+
             tot_lenght_ = side_lenght;
+            lattice_.reserve(tot_lenght_);
+            nearest_neighbors_.reserve(tot_lenght_);
+            nearest_list.reserve(2);
+
             for (int i = 0; i < tot_lenght_; i++){
                 nearest_list.clear();
                 nearest_list.push_back(i - 1);
                 nearest_list.push_back(i + 1);
+
+                // Implementing PBC
                 if (i == 0){
-                  nearest_list[0] = tot_lenght_-1;
+                    /* First element */
+                    nearest_list[0] = tot_lenght_-1;
                 } else if (i == tot_lenght_-1){
-                  nearest_list[1] = 0;
+                    /* Last element */
+                    nearest_list[1] = 0;
                 }
+
+                // Push the result
                 nearest_neighbors_.push_back(nearest_list);
             }
+            nearest_list.clear();
 
         } else if (geometry_flag == 1) {
-            /* Initialising 2D square lattice and nearest neighbors */
+            /* 2D square lattice with PBC */
+
             tot_lenght_ = side_lenght * side_lenght;
+            lattice_.reserve(tot_lenght_);
+            nearest_neighbors_.reserve(tot_lenght_);
+            nearest_list.reserve(4);
+
+            for (int i = 0; i < tot_lenght_; i++){
+                nearest_list.clear();
+                nearest_list.push_back(i - 1);
+                nearest_list.push_back(i + 1);
+                nearest_list.push_back(i - side_lenght);
+                nearest_list.push_back(i + side_lenght);
+
+                // Implementing PBC
+                if (i % side_lenght  == 0){
+                    /* First culomn */
+                    nearest_list[0] += side_lenght;
+                } else if ((i + 1) % side_lenght  == 0){
+                    /* Last culomn*/
+                    nearest_list[1] -= side_lenght;
+                }
+
+                if (i < side_lenght){
+                    /* First row */
+                    nearest_list[2] += tot_lenght_;
+                } else if (i >= tot_lenght_ - side_lenght){
+                    /* Last row */
+                    nearest_list[3] -= tot_lenght_;
+                }
+
+                // Push the result
+                nearest_neighbors_.push_back(nearest_list);
+            }
+            nearest_list.clear();
+
         } else {
             /* Not implemented */
+
+            cerr << "Error: geometry not implemented." << endl;
+            exit(1);
         }
 
-        lattice_.reserve(tot_lenght_);
-
-        if (temperature_flag == 0) {
+        // Initialising the lattice configuration
+        if (initial_flag == 0) {
             /* Initialising cold lattice */
+
             for (int i = 0; i < tot_lenght_; i++){
-                lattice_.push_back(0.);
+                lattice_.push_back(1);
             }
 
-        } else if (temperature_flag == 1) {
+        } else if (initial_flag == 1) {
             /* Initialising hot lattice */
+
             for (int i = 0; i < tot_lenght_; i++){
-                lattice_.push_back(1.);
+                random_number = prng(generator);
+                if (random_number < 0.5){
+                    lattice_.push_back(0);
+                } else {
+                    lattice_.push_back(1);
+                }
+            }
+
+        } else if (initial_flag == 2) {
+            /* Restore old lattice from file */
+
+            cout << "Loading initial configuration..." << endl;
+            ifstream file("lattice_configuration.dat");
+            if (file.is_open()) {
+                int site;
+                while (file >> site){
+                    lattice_.push_back(site);
+                }
+                file.close();
+            } else {
+                cerr << "Error: unable to open the file." << endl;
+                exit(1);
+            }
+
+            if (lattice_.size() != tot_lenght_){
+                cerr << "Error: different number of sites." << endl;
+                exit(1);
             }
 
         } else {
             /* Not implemented */
+
+            cerr << "Error: initial configuration not implemented." << endl;
+            exit(1);
         }
 
-    }
+    }/************************* Class methods *********************************/
 
     void show_configuration(){
+        /* Print the lattice configuration */
+
         cout << "Lattice configuration: " << endl;
-        for (int i = 0; i < tot_lenght_; i++){
-            cout << lattice_[i] << " ";
+
+        if (geometry_flag == 0){
+            for (int i = 0; i < tot_lenght_; i++){
+                cout << lattice_[i] << " ";
+            }
+            cout << endl << endl;
+
+        } else if (geometry_flag == 1){
+            for (int i = 0; i < tot_lenght_; i++){
+                cout << lattice_[i] << " ";
+                if ((i + 1) % side_lenght == 0){
+                    cout << endl;
+                }
+            }
+            cout << endl;
         }
-        cout << endl;
+    }
+
+    void save_configuration(){
+        /* Save the current configuration of the lattice */
+
+        ofstream file;
+        file.open ("lattice_configuration.dat");
+        for (int i = 0; i < tot_lenght_; i++){
+            file << lattice_[i] << endl;
+        }
+        file.close();
     }
 
     void show_nearest_index(const int &index){
+        /* Print the nearest neighbors of the site index */
+
         int size;
         size = nearest_neighbors_[index].size();
 
@@ -92,13 +209,16 @@ public:
     }
 
     void show_nearest_neighbors(){
+        /* Print the list of all nearest neighbors to each lattice site */
+
         for (int i = 0; i < tot_lenght_; i++){
-            cout << "The nearest neighbors to element " << i << " are: ";
+            cout << "Nearest neighbors to " << i << " are: ";
             show_nearest_index(i);
             cout << endl;
         }
     }
 
-};
+};/************************* End class ****************************************/
 
+//------------------------------------------------------------------------------
 #endif
--- 1.Monte_Carlo/Ising/ising_main.cpp
+++ 1.Monte_Carlo/Ising/ising_main.cpp
@@ -1,29 +1,37 @@
-/**
-* Main program for the Ising simulation bla bla bla
-*/
+/*******************************************************************************
+*
+* Main program for the Ising simulation
+*
+*******************************************************************************/
 
-//----Preprocessor directives--------------------------------------
+//----Preprocessor directives---------------------------------------------------
 #include <iostream>
-#include <fstream>       // file stream
+#include <fstream>
 #include <random>        // import mt19937 periodo di 2^19937 − 1
 #include <cmath>
 
 #include "ising_lattice.h"
 
-#define SEED 42
-
-#define L 5
-#define G_FLAG 0
-#define T_FLAG 0
+/*
+* SIDE = size of the lattice's side.
+* G_FLAG = geometry flag; 0 for 1D periodic chain, 1 for 2D square with PBC.
+* I_FLAG = initial configuration flag; 0 for cold initialization, 1 for hot
+*   (random) initialization, 2 for loading the previous configuration from file.
+*/
+#define SIDE 5
+#define G_FLAG 1
+#define I_FLAG 1
 
 using namespace std;
 
-//----Contents-----------------------------------------------------
+//----Contents------------------------------------------------------------------
 
 int main(){
-    lattice ising(L, G_FLAG, T_FLAG);
+    lattice ising(SIDE, G_FLAG, I_FLAG);
 
     ising.show_configuration();
 
-    ising.show_nearest_neighbors();
+    ising.save_configuration();
+
+    // ising.show_nearest_neighbors();
 }
