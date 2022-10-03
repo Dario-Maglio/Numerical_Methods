143ba6508ff9b007f818017994cf1b4d0501e285
Dario-Maglio <cafasso.dario@gmail.com>
2022-10-03 22:27:05 +0300

    autocorrelation and tau_int added to metropolis

--- 1.Monte_Carlo/Metropolis/metropolis.cpp
+++ 1.Monte_Carlo/Metropolis/metropolis.cpp
@@ -10,9 +10,12 @@
 #include <fstream>       // file stream
 #include <random>        // import mt19937 periodo di 2^19937 − 1
 #include <cmath>
+#include <vector>
 
 #define N_STEPS 1015000
 #define DIM_SAMPLING 1000
+#define DIM_SUBSAMP 1000000
+#define DIM_THERM 10000
 #define SEED 42
 
 #define AVERAGE 5.0
@@ -22,7 +25,7 @@
 
 using namespace std;
 
-//----Contents------------------------------------------------------------------
+//---- Contents ----------------------------------------------------------------
 
 /* Probability ratio function */
 inline double prf(double q, double q_try) {
@@ -30,11 +33,64 @@ inline double prf(double q, double q_try) {
     return exp(q / (2*pow(SIGMA, 2)));
 }
 
+/* Autocorrelation function, variance and error */
+void auto_corre(int dim_subsample){
+    int steps = 0;
+    double value, x_ave = 0., tau_int = 0., var_bias = 0., sigma;
+    vector<double> x;
 
+    // load the data file and compute the average
+    ifstream data("f_metropolis.dat");
+    if (data.is_open()) {
+        while ((data >> value) && (steps < DIM_THERM)) steps += 1;
+        steps = 0;
+        while ((data >> value) && (steps <= dim_subsample)){
+            steps += 1;
+            x_ave += value;
+            x.push_back(value);
+        }
+        data.close();
+        x_ave = x_ave / steps;
+    } else {
+        cerr << "Error: unable to open the file." << endl;
+        exit(1);
+    }
+    cout << "The average of the sample is " << x_ave << endl;
 
-/* Main program */
-int main() {
-    double x, y, q_try, q=START, sample_average=0., sample_ave_average=0.;
+    // compute the biased variance
+    for (int k = 0; k < steps; k++) var_bias += pow(x[k] - x_ave , 2);
+    var_bias = (var_bias/steps)  / (steps - 1);
+    cout << "The biased std deviation is " << sqrt(var_bias) << endl;
+
+    // compute tau_int and the autocorrelation function
+    ofstream file_corr, file_tau;
+    file_corr.open("f_autocorr.dat");
+    file_tau.open("f_tau_int.dat");
+
+    for (int k = 0; k < steps; k++){
+        value = 0.;
+        for (int i = 0; i < (steps - k); i++){
+            value += pow(x_ave, 2) + (x[i] * x[i+k]) - x_ave * (x[i] + x[i+k]);
+        }
+        value = value / (steps - k);
+        file_corr << value << endl;
+        tau_int += value;
+        file_tau << tau_int << endl;
+
+        // the approximation starts breaking down; steps must be >> t_int
+        if (value < 0) break;
+    }
+
+    file_corr.close();
+    file_tau.close();
+
+    sigma = sqrt(var_bias * (1 + 2*tau_int));
+    cout << "The result is " << x_ave << " +- " << sigma << endl;
+}
+
+/* generate the MC data */
+void generate_MC_data() {
+    double x, y, q_try, q=START, sample_average=0.;
 
     // create files
     ofstream file_m, file_a;
@@ -61,19 +117,25 @@ int main() {
         sample_average = sample_average + q;
         file_m << q << endl;
 
-        // calculate the average average
+        // compute the average over a sample
       	if ((it % DIM_SAMPLING) == 0) {
 	         sample_average = sample_average / DIM_SAMPLING;
 	         file_a << sample_average << endl;
-	         sample_ave_average = sample_ave_average + sample_average;
 	         sample_average=0.;
          	}
     }
 
-    cout << sample_ave_average / (N_STEPS / DIM_SAMPLING)  << endl;
-
     // close files
     file_m.close();
     file_a.close();
+}
 
+//---- Main --------------------------------------------------------------------
+
+int main() {
+    // generate_MC_data();
+
+    auto_corre(DIM_SUBSAMP);
+
+    return 0;
 }
--- 1.Monte_Carlo/Metropolis/pythoninterface.py
+++ 1.Monte_Carlo/Metropolis/pythoninterface.py
@@ -3,54 +3,78 @@ import os
 import numpy as np
 import matplotlib.pyplot as plt
 
-# os.system('g++ metropolis.cpp -o main')
-# os.system('./main')
-# os.system('rm main')
+
 
 #-------------------------------------------------------------------------------
 
-a = np.loadtxt('f_metropolis.dat', unpack='True')
+def plot_metropolis():
+    a = np.loadtxt('f_metropolis.dat', unpack='True')
 
-x = [i+1 for i in range(15000)]
-y1 = a[:15000]
-y2 = a[15000:]
+    x = [i+1 for i in range(15000)]
+    y1 = a[:15000]
+    y2 = a[15000:]
 
-plt.figure("MC evolution gaussian")
-plt.title("Thermalization" + "\n" + "Sequence of 15000 Metropolis steps")
-plt.plot(x, y1, marker='.', markersize=0.1, label='MC points')
-plt.xlabel('Step')
-plt.ylabel('Evolution')
-plt.legend(loc='lower right')
+    plt.figure("MC evolution gaussian")
+    plt.title("Thermalization" + "\n" + "Sequence of 15000 Metropolis steps")
+    plt.plot(x, y1, marker='.', markersize=0.1, label='MC points')
+    plt.xlabel('Step')
+    plt.ylabel('Evolution')
+    plt.legend(loc='lower right')
 
-plt.figure("MC histogram gaussian")
-plt.title("Histogram of the MC steps" + "\n" + "average = 5. , sigma = 1.")
-plt.hist(y2, bins=500)
-plt.ylabel('Counting')
-plt.xlabel('MC step')
+    plt.figure("MC histogram gaussian")
+    plt.title("Histogram of the MC steps" + "\n" + "average = 5. , sigma = 1.")
+    plt.hist(y2, bins=500)
+    plt.ylabel('Counting')
+    plt.xlabel('MC step')
 
-plt.show()
+    plt.show()
+
+def plot_averages():
+    a = np.loadtxt('f_averages.dat', unpack='True')
+    x = [i+1 for i in range(len(a))]
+
+    a = a[15:]
+    x = x[15:]
+
+    plt.figure("MC averages gaussian")
+    plt.title("Scatter of the sample averages")
+    plt.scatter(x, a, marker='.')
+    plt.ylabel('Average')
+    plt.ylim([0, 7.1])
+    plt.xlabel('Sample')
+
+    plt.show()
+
+def plot_autocorr():
+    a = np.loadtxt('f_autocorr.dat', unpack='True')
+    x = [i+1 for i in range(len(a))]
+
+    plt.figure("MC autocorrelation")
+    plt.title("Autocorrelation of a 1000000 steps sample")
+    plt.plot(x, a, marker='.', markersize=0.1)
+    plt.ylabel('C(k)')
+    plt.xlabel('k')
+
+    a = np.loadtxt('f_tau_int.dat', unpack='True')
+    x = [i+1 for i in range(len(a))]
+
+    plt.figure("MC autocorrelation time")
+    plt.title("Integrated autocorrelation time")
+    plt.plot(x, a, marker='.', markersize=0.1)
+    plt.ylabel('tau_int(k)')
+    plt.xlabel('k')
+
+    plt.show()
 
 #-------------------------------------------------------------------------------
 
-a = np.loadtxt('f_averages.dat', unpack='True')
-x = [i+1 for i in range(len(a))]
+if __name__ == '__main__':
+    os.system('g++ metropolis.cpp -o main')
+    os.system('./main')
+    os.system('rm main')
 
-a = a[15:]
-x = x[15:]
+    # plot_metropolis()
 
-plt.figure("MC averages gaussian")
-plt.title("Scatter of the sample averages")
-plt.scatter(x, a, marker='.')
-plt.ylabel('Average')
-plt.ylim([0, 7.1])
-plt.xlabel('Sample')
+    # plot_averages()
 
-plt.show()
-
-#-------------------------------------------------------------------------------
-
-y1 = y1[5000:15000]
-print("10000 steps sample")
-print(f"The average is {np.average(y1)} +- {np.sqrt(np.var(y1)/(len(y1) - 1))}")
-print("Complete sample")
-print(f"The average is {np.average(y2)} +- {np.sqrt(np.var(y2)/(len(y2) - 1))}")
+    plot_autocorr()
