bf65922194db3f979d04e2763d02b9d712015356
Dario-Maglio <cafasso.dario@gmail.com>
2022-10-04 23:51:40 +0300

    update metropolis, dataset and graphs

--- 1.Monte_Carlo/Metropolis/metropolis.cpp
+++ 1.Monte_Carlo/Metropolis/metropolis.cpp
@@ -13,10 +13,11 @@
 #include <vector>
 
 #define SEED 42
-#define N_STEPS 1015000            // dim of sample to generate
-#define DIM_SAMPLING 1000          // dim of subsamples for calculating averages
-#define DIM_SUBSAMP 1000000        // dim of subsample for autocorrelation
-#define DIM_THERM 10000            // dim of subsample for thermalization
+#define N_STEPS 10015000           // dim of sample to generate
+#define DIM_SAMPLING 10000         // dim of subsamples for calculating averages
+#define DIM_SUBSAMP 4194304        // dim of subsample for autocorrelation 2^22
+#define DIM_THERM 15000            // dim of subsample for thermalization
+#define NUM_FAKESAMPLES 100        // num of fake samples
 
 #define AVERAGE 5.0
 #define SIGMA 1.
@@ -24,6 +25,11 @@
 #define DELTA 0.1                  // delta circa 3-4 sigma gives the best err
 
 using namespace std;
+
+// define the PRNG
+mt19937_64 generator(SEED);
+uniform_real_distribution<double> prng(0.0, 1.0);
+uniform_int_distribution randomint(0, DIM_SUBSAMP);
 
 //---- Contents ----------------------------------------------------------------
 
@@ -41,10 +47,6 @@ void generate_MC_data() {
     ofstream file_m, file_a;
     file_m.open("file_metropol.dat");
     file_a.open("file_averages.dat");
-
-    // define the PRNG
-    mt19937 generator(SEED);
-    uniform_real_distribution<double> prng(0.0, 1.0);
 
     // Metropolis-Hastings
     for(int it=1; it<=N_STEPS; it++){
@@ -76,7 +78,7 @@ void generate_MC_data() {
 }
 
 /* Load the sample data and compute the average */
-float load_and_average(int dim_therm, int dim_subsample, vector<double>& x){
+double load_and_average(int dim_therm, int dim_subsample, vector<double>& x){
   int steps = 0;
   double value, x_ave = 0.;
 
@@ -101,55 +103,167 @@ float load_and_average(int dim_therm, int dim_subsample, vector<double>& x){
 }
 
 /* Autocorrelation function, variance and error */
-float auto_corre(vector<double>& x, float x_ave){
-    int steps = size(x);
-    double value, tau_int = 0., var_bias = 0., sigma;
+double autocorrelation(vector<double>& x, float x_ave){
+    int k = 0, steps = size(x);
+    double value = 1., tau_int = 0., var_bias = 0., sigma;
 
     // compute the biased variance
-    for (int k = 0; k < steps; k++) var_bias += pow(x[k] - x_ave , 2);
+    for (int j = 0; j < steps; j++) var_bias += pow(x[j] - x_ave , 2);
     var_bias = var_bias / (steps - 1);
     cout << "logging: biased std dev is " << sqrt(var_bias/steps) << endl;
 
     // compute tau_int and the autocorrelation function
-    ofstream file_cor, file_tau;
-    file_cor.open("file_autocor.dat");
-    file_tau.open("file_tau_int.dat");
+    ofstream file;
+    file.open("file_autocor.dat");
 
-    for (int k = 0; k < steps; k++){
+    while (value > 0.) {
         value = 0.;
         for (int i = 0; i < (steps - k); i++){
             value += pow(x_ave, 2) + (x[i] * x[i+k]) - x_ave * (x[i] + x[i+k]);
         }
         value = value / (steps - k);
-        file_cor << value << endl;
         tau_int += value;
-        file_tau << tau_int << endl;
+        k++;
 
-        // the approximation starts breaking down; steps must be >> t_int
-        if (value < 0) break;
+        file << k << " " << value << " " << tau_int << endl;
+        cout << "logging: C at " << k << " is " << value << endl;
     }
-
-    file_cor.close();
-    file_tau.close();
+    file.close();
 
     sigma = (1 + 2*tau_int) / steps;
     sigma = sqrt(var_bias * sigma);
     return sigma;
 }
 
+/* Bootstrap without autocorrelation */
+double bootstrap_step(vector<double>& x){
+    int steps = size(x);
+    double x_fake, fake_ave = 0.;
+    vector<double> fake;
+    fake.reserve(steps);
+
+    for (int i = 0; i < steps; i++){
+        x_fake = x[randomint(generator)];
+        fake_ave += x_fake;
+        fake.push_back(x_fake);
+    }
+    fake.clear();
+
+    fake_ave = fake_ave / steps;
+    return fake_ave;
+}
+
+double bootstrap(vector<double>& x, int num_fake_samples){
+    double fake_ave, fake_ave_ave = 0., sigma = 0.;
+    vector<double> fake_averages;
+    fake_averages.reserve(num_fake_samples);
+
+    ofstream file_boot;
+    file_boot.open("file_boot.dat");
+
+    for (int k = 5; k <= num_fake_samples; k += 5){
+        fake_ave_ave = 0.;
+        for (int i = 0; i < k; i++){
+            fake_ave = bootstrap_step(x);
+            fake_ave_ave += fake_ave;
+            fake_averages.push_back(fake_ave);
+        }
+        fake_ave_ave = fake_ave_ave / k;
+
+        sigma = 0.;
+        for (int i = 0; i < k; i++){
+            sigma += pow(fake_averages[i], 2);
+        }
+        sigma = sigma / k;
+        sigma = sigma - pow(fake_ave_ave, 2);
+        sigma = sqrt(sigma);
+
+        fake_averages.clear();
+        file_boot << k << " " << sigma << endl;
+        cout << "logging: sigma at " << k << " is " << sigma << endl;
+    }
+
+    file_boot.close();
+    return sigma;
+}
+
+/* Bootstrap with autocorrelation */
+double bootstrap_corr_step(vector<double>& x, int k){
+    int index, num_block, steps = size(x);
+    double x_fake, fake_ave = 0.;
+    vector<double> fake;
+    fake.reserve(steps);
+
+    num_block = steps / pow(2, k);
+
+    for (int i = 0; i < num_block; i++){
+        index = randomint(generator);
+        for (int j = 0; j < pow(2, k); j++){
+            if (index + j == steps) index = index - steps;
+            x_fake = x[index + j];
+            fake_ave += x_fake;
+            fake.push_back(x_fake);
+        }
+    }
+    fake.clear();
+
+    fake_ave = fake_ave / steps;
+    return fake_ave;
+}
+
+double bootstrap_corr(vector<double>& x, int num_fake_samples){
+    double fake_ave, fake_ave_ave = 0., sigma = 0.;
+    vector<double> fake_averages;
+    fake_averages.reserve(num_fake_samples);
+
+    ofstream file_boot;
+    file_boot.open("file_boot_corr.dat");
+
+    for (int k = 5; k < 16; k++){
+        fake_ave_ave = 0.;
+        for (int i = 0; i < num_fake_samples; i++){
+            fake_ave = bootstrap_corr_step(x, k);
+            fake_ave_ave += fake_ave;
+            fake_averages.push_back(fake_ave);
+        }
+        fake_ave_ave = fake_ave_ave / num_fake_samples;
+
+        sigma = 0.;
+        for (int i = 0; i < num_fake_samples; i++){
+            sigma += pow(fake_averages[i], 2);
+        }
+        sigma = sigma / num_fake_samples;
+        sigma = sigma - pow(fake_ave_ave, 2);
+        sigma = sqrt(sigma);
+
+        fake_averages.clear();
+        file_boot << k << " " << sigma << endl;
+        cout << "logging: sigma at " << k << " is " << sigma << endl;
+    }
+
+    file_boot.close();
+    return sigma;
+}
+
 //---- Main --------------------------------------------------------------------
 
 int main() {
-    float x_ave, sigma;
+    double x_ave, sigma;
     vector<double> data;
 
-    generate_MC_data();
+    // generate_MC_data();
 
     x_ave = load_and_average(DIM_THERM, DIM_SUBSAMP, data);
     cout << "The average of the sample is " << x_ave << endl;
 
-    sigma = auto_corre(data, x_ave);
-    cout << "The result is " << x_ave << " +- " << sigma << endl;
+    // sigma = autocorrelation(data, x_ave);
+    // cout << "The result is " << x_ave << " +- " << sigma << endl;
+
+    sigma = bootstrap(data, NUM_FAKESAMPLES);
+    cout << "Bootstrap gives: " << sigma << endl;
+
+    sigma = bootstrap_corr(data, NUM_FAKESAMPLES);
+    cout << "Bootstrap corr gives: " << sigma << endl;
 
     return 0;
 }
--- 1.Monte_Carlo/Metropolis/pythoninterface.py
+++ 1.Monte_Carlo/Metropolis/pythoninterface.py
@@ -10,56 +10,81 @@ THERMALIZATION = 15000
 def plot_metropolis():
     a = np.loadtxt('file_metropol.dat', unpack='True')
 
-    x = [i+1 for i in range(THERMALIZATION)]
+    x = [i for i in range(THERMALIZATION)]
     y1 = a[:THERMALIZATION]
     y2 = a[THERMALIZATION:]
 
-    plt.figure("MC evolution gaussian")
-    plt.title("Thermalization" + "\n" + "Sequence of 15000 Metropolis steps")
-    plt.plot(x, y1, marker='.', markersize=0.1, label='MC points')
-    plt.xlabel('Step')
-    plt.ylabel('Evolution')
-    plt.legend(loc='lower right')
+    fig, axes = plt.subplots(1, 2, num="MC gaussian", figsize=(12, 12))
 
-    plt.figure("MC histogram gaussian")
-    plt.title("Histogram of the MC steps" + "\n" + "average = 5. , sigma = 1.")
-    plt.hist(y2, bins=500)
-    plt.ylabel('Counting')
-    plt.xlabel('MC step')
+    axes[0].set_title("Thermalization phase \n sequence of 15000 steps")
+    axes[0].plot(x, y1, marker='.', markersize=0.01,
+                 label='start = 0 , delta = 0.1')
+    axes[0].set_ylabel('Evolution')
+    axes[0].set_xlabel('Step')
+    axes[0].legend(loc='lower right')
+
+    axes[1].set_title("Histogram after thermalization \n aver = 5 , sigma = 1")
+    axes[1].hist(y2, bins=500, density=True)
+    axes[1].set_ylabel('Counting')
+    axes[1].set_xlabel('Variable')
 
     plt.show()
 
 def plot_averages():
     a = np.loadtxt('file_averages.dat', unpack='True')
-    x = [i+1 for i in range(len(a))]
+    x = [i for i in range(len(a))]
 
-    plt.figure("MC averages gaussian")
-    plt.title("Scatter of the sample averages")
+    plt.figure("MC gaussian averages")
+    plt.title("Sample averages \n dim subsamples = 10000")
     plt.scatter(x, a, marker='.')
+    plt.ylim([0, 7])
     plt.ylabel('Average')
-    plt.ylim([0, 7.1])
     plt.xlabel('Sample')
 
     plt.show()
 
 def plot_autocorr():
     a = np.loadtxt('file_autocor.dat', unpack='True')
-    x = [i+1 for i in range(len(a))]
+    x = a[0, :]
+    c = a[1, :]
+    t = a[2, :]
 
-    plt.figure("MC autocorrelation")
-    plt.title("Autocorrelation of a 1000000 steps sample")
-    plt.plot(x, a, marker='.', markersize=0.1)
-    plt.ylabel('C(k)')
-    plt.xlabel('k')
+    fig, axes = plt.subplots(1, 2, sharex=True, num="MC autocorrelation")
 
-    a = np.loadtxt('file_tau_int.dat', unpack='True')
-    x = [i+1 for i in range(len(a))]
+    axes[0].set_title("Autocorrelation \n k = |j - i|")
+    axes[0].plot(x, c, marker='.', markersize=0.1)
+    axes[0].set_ylabel('C(k)')
 
-    plt.figure("MC autocorrelation time")
-    plt.title("Integrated autocorrelation time")
-    plt.plot(x, a, marker='.', markersize=0.1)
-    plt.ylabel('tau_int(k)')
-    plt.xlabel('k')
+    axes[1].set_title("Integrated autocorrelation time \n dim sample = 2^22")
+    axes[1].plot(x, t, marker='.', markersize=0.1)
+    axes[1].set_ylabel('tau_int(k)')
+
+    plt.show()
+
+def plot_bootstrap():
+    a = np.loadtxt('file_boot.dat', unpack='True')
+    x = a[0, :]
+    y = a[1, :]
+
+    plt.figure("MC bootstrap without corr")
+    plt.title("Bootstrap \n k = number of fake samples, dim samples = 2^22")
+    plt.plot(x, y, marker='.', markersize=0.1)
+    plt.ylabel('sigma(k)')
+    plt.xlabel('step k')
+
+    plt.show()
+
+def plot_bootstrap_corr():
+    a = np.loadtxt('file_boot_corr.dat', unpack='True')
+    x = a[0, :]
+    y = a[1, :]
+
+    plt.figure("MC bootstrap with corr")
+    plt.title("Bootstrap with correlations \n" +
+              "correl block dim = 2^k, dim sample = 2^22, num samples = 100")
+    plt.plot(x, y, marker='.', markersize=0.1)
+    plt.ylabel('sigma(k)')
+    plt.xlabel('step k')
 
     plt.show()
 
@@ -75,3 +100,7 @@ if __name__ == '__main__':
     plot_averages()
 
     plot_autocorr()
+
+    plot_bootstrap()
+
+    plot_bootstrap_corr()
